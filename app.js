22afa4da41db2c787b08db207a4d1735c9a2933f
ahiajalil <ahiajalil@gmail.com>
2023-07-11 00:11:23 +0100

    added javascript

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-const days = document.querySelectorAll("#days");
-const hours = document.querySelectorAll("#hours");
-const minutes = document.querySelectorAll("#minutes");
-const seconds = document.querySelectorAll("#seconds");
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+let launchDate = new Date("Aug 01, 2023 00:00:00").getTime();
+
+let tracking = setInterval(function () {
+  let current = new Date().getTime();
+  let timeleft = launchDate - current;
+
+  let day = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+  let hour = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let mintue = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+  let second = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+  days.innerHTML = day;
+  hours.innerHTML = hour;
+  minutes.innerHTML = mintue;
+  seconds.innerHTML = second;
+}, 1000);
